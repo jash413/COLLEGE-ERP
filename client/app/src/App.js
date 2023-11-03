acf2004115e65cb571379899c7406fc74a5f0ee8
jash413 <jashmistry4444@gmail.com>
2023-11-04 00:10:38 +0530

    changes

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -1,11 +1,13 @@
 import SideBar from './components/SideBar';
 import Header from './components/Header';
+import StudentAdd from './pages/StudentAdd';
 
 function App() {
   return (
     <div className="App">
       <SideBar />
       <Header />
+      <StudentAdd />
     </div>
   );
 }
